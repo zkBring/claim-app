258453f0c81bd3c059ebbd0da5ae721ea100a1f7
spacehaz <spacehaz@gmail.com>
2023-03-21 16:26:10 +0100

    update factory

--- src/configs/contracts.tsx
+++ src/configs/contracts.tsx
@@ -12,7 +12,7 @@ const contracts: TContracts = {
     factory: '0xD61C4f3834480fECaA2EdcF0006FfB3005daE300'
   }, // goerli
   137: {
-    factory: '0x18165C54C28cF298059d55CA535529a4958852af'
+    factory: '0x50dADaF6739754fafE0874B906F60688dB483855'
   }, // polygon
   80001: {
     factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53'
