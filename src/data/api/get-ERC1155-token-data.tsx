c57610f1928ec403811848e0951bd3a5a4e189b9
spacehaz <spacehaz@gmail.com>
2023-03-22 17:23:32 +0100

    added new sdk and updates for env variables

--- src/data/api/get-ERC1155-token-data.tsx
+++ src/data/api/get-ERC1155-token-data.tsx
@@ -3,7 +3,7 @@ import { metadataUrlResolve } from 'helpers'
 
 const getERC1155TokenData = (url: string, tokenId: string) => {
   const tokenDataURL = url.replace('0x{id}', tokenId)
-  return axios(metadataUrlResolve(tokenDataURL, tokenId))
+  return axios(metadataUrlResolve(tokenDataURL, tokenId), { timeout: 1500 })
 }
 
 export default getERC1155TokenData
