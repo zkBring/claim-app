5eb69deb801b9ccbf0beb4228ac02dff60f89300
spacehaz <spacehaz@gmail.com>
2023-03-22 16:25:57 +0100

    fixes for factory

--- src/configs/contracts.tsx
+++ src/configs/contracts.tsx
@@ -6,16 +6,16 @@ type TContracts = {
 
 const contracts: TContracts = {
   1: {
-    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53'
+    factory: '0x50dADaF6739754fafE0874B906F60688dB483855'
   }, // mainnet
   5: {
-    factory: '0xD61C4f3834480fECaA2EdcF0006FfB3005daE300'
+    factory: '0x50dADaF6739754fafE0874B906F60688dB483855'
   }, // goerli
   137: {
     factory: '0x50dADaF6739754fafE0874B906F60688dB483855'
   }, // polygon
   80001: {
-    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53'
+    factory: '0x50dADaF6739754fafE0874B906F60688dB483855'
   }
 
 }
