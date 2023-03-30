2a57c6de5eb6bd046f0f2612c524c0c9771a4679
spacehaz <spacehaz@gmail.com>
2023-03-30 10:54:39 +0200

    update zerion deeplink

--- src/components/pages/claim-page/wallets-list/index.tsx
+++ src/components/pages/claim-page/wallets-list/index.tsx
@@ -249,7 +249,7 @@ const WalletsList: FC<ReduxType> = ({
         if (!uri) { return }
         setLoading(true)
         const href = `https://wallet.zerion.io/wc?uri=${encodeURIComponent(uri)}`
-        window.location.href = href
+        window.open(href, '_blank')
       })
       .catch(err => {
         setLoading(false)
