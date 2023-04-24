9822ed6f7fde4ee11263a52029948c5c5a1e13c4
spacehaz <spacehaz@gmail.com>
2023-04-24 17:47:01 +0200

    new screens for deeplink usage

--- src/helpers/copy-to-clipboard.tsx
+++ src/helpers/copy-to-clipboard.tsx
@@ -1,16 +1,63 @@
-import defineNetworkName from './define-network-name'
-
-type TDefineNFTExplorerUrl = ({ chainId, tokenId, tokenAddress }: { chainId: number, tokenId: string, tokenAddress: string }) => string
-
-const defineOpenseaURL: TDefineNFTExplorerUrl = ({ chainId, tokenId, tokenAddress }) => {
-  const networkName = defineNetworkName(chainId)
-  if (networkName === 'mainnet') {
-    return `https://opensea.io/assets/${tokenAddress}/${tokenId}`
+const copyToClipboard = ({ value }: { value: Record<string, any> | string }) => {
+  let textArea: HTMLTextAreaElement
+  function isOS () {
+    return navigator.userAgent.match(/ipad|iphone/i)
   }
-  if (networkName === 'polygon') {
-    return `https://opensea.io/assets/matic/${tokenAddress}/${tokenId}`
+
+  function createTextArea (data: Record<string, any> | string) {
+    textArea = document.createElement('textarea')
+    const valueToCopy = typeof data === 'string' ? data : Object.keys(data).reduce((sum, item, idx, arr) => {
+      if (idx === 0) {
+        sum = '{'
+      } else {
+        sum = `${sum},`
+      }
+      sum = `${sum} ${item}: ${data[item]}`
+      if (idx === arr.length - 1) {
+        sum = `${sum} }`
+      }
+      return sum
+    }, '')
+
+    textArea.value = valueToCopy
+
+    textArea.style.position = 'fixed'
+    textArea.style.left = '50%'
+    textArea.style.top = '50%'
+    textArea.style.transform = 'translate(-50%, -50%)'
+    textArea.onfocus = (e) => {
+      e.preventDefault()
+    }
+    document.body.appendChild(textArea)
   }
-  return `https://testnets.opensea.io/assets/${networkName}/${tokenAddress}/${tokenId}`
+
+  function selectText () {
+    var range,
+    selection
+
+    if (isOS()) {
+      range = document.createRange()
+      range.selectNodeContents(textArea)
+      selection = window.getSelection()
+      if (!selection) return
+      selection.removeAllRanges()
+      selection.addRange(range)
+      textArea.setSelectionRange(0, 999999)
+    } else {
+      textArea.select()
+    }
+  }
+
+  function copyToClipboard () {
+    document.execCommand('copy')
+    textArea.blur()
+    document.body.removeChild(textArea)
+  }
+
+  createTextArea(value)
+  selectText()
+  copyToClipboard()
 }
 
-export default defineOpenseaURL
+
+export default copyToClipboard
