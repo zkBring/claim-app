b86a0bdbfa660bc9d6b2fcb6d7f38e199b2d0474
spacehaz <spacehaz@gmail.com>
2023-05-02 18:54:38 +0200

    added update for coinbase wallet

--- src/components/application/connectors/wallet-connect.tsx
+++ src/components/application/connectors/wallet-connect.tsx
@@ -4,21 +4,39 @@ import {
   w3mProvider
 } from "@web3modal/ethereum"
 import { configureChains, createClient } from "wagmi"
-import { mainnet, polygon } from "wagmi/chains"
+import { mainnet, polygon, goerli, polygonMumbai } from "wagmi/chains"
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet"
 const { REACT_APP_WC_PROJECT_ID } = process.env
 
-const chains = [polygon, mainnet]
+const chains = [mainnet, polygon, goerli, polygonMumbai]
 
 // Wagmi client
-const { provider } = configureChains(chains, [w3mProvider({ projectId: REACT_APP_WC_PROJECT_ID as string })]);
+const { provider } = configureChains(chains, [
+  w3mProvider({
+    projectId: REACT_APP_WC_PROJECT_ID as string
+  })
+])
 
 const wagmiClient = createClient({
-  connectors: w3mConnectors({
-    projectId: REACT_APP_WC_PROJECT_ID as string,
-    version: 2,
-    chains
-  }),
-  provider
+  connectors: [
+    new WalletConnectConnector({
+      chains, options: {
+        projectId: REACT_APP_WC_PROJECT_ID as string
+      }
+    }),
+    new InjectedConnector({
+      chains
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: "wagmi demo",
+      },
+    }),
+  ],
+  provider,
 })
 
 const ethereumClient = new EthereumClient(wagmiClient, chains)
