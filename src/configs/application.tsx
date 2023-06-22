11f20ddf5275b1bf1a44207601b9983de476b627
spacehaz <spacehaz@gmail.com>
2023-06-22 15:31:48 +0200

    added update for configs

--- src/configs/application.tsx
+++ src/configs/application.tsx
@@ -6,6 +6,8 @@ type TApplicationParams = {
   primaryTextColor: string
   logo: string
   backgroundColor: string
+  highlightHoverColor: string
+  highlightActiveColor: string
 }
 
 type TConfig = Record<string, TApplicationParams>
@@ -15,14 +17,18 @@ const originalParams: TApplicationParams = {
   highlightColor: '#0C5EFF',
   primaryTextColor: '#121212',
   logo: LinkdropLogo,
-  backgroundColor: '#F7F7FB'
+  backgroundColor: '#F7F7FB',
+  highlightHoverColor: '#357AFF',
+  highlightActiveColor: '#095AF5'
 }
 
 const lumasParams: TApplicationParams = {
   highlightColor: '#8719CB',
   primaryTextColor: '#121212',
   logo: LumasLogo,
-  backgroundColor: '#F7F7FB'
+  backgroundColor: '#F7F7FB',
+  highlightHoverColor: '#6A149F',
+  highlightActiveColor: '#6A149F'
 }
 
 const applicationParams: TConfig = {
