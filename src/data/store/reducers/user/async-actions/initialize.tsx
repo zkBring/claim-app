fcf41ba344836711533f3598e0c77483eba7941b
spacehaz <spacehaz@gmail.com>
2023-07-13 13:46:01 +0200

    added updates for claim check

--- src/data/store/reducers/user/async-actions/initialize.tsx
+++ src/data/store/reducers/user/async-actions/initialize.tsx
@@ -11,89 +11,90 @@ import {
 } from 'helpers'
 const { REACT_APP_INFURA_ID } = process.env
 
-const initialize = async (
-  dispatch: Dispatch<UserActions> & Dispatch<DropActions>,
-  getState: () => RootState,
+const initialize = (
   onReload: () => void,
   connector: any,
   userChainId?: number,
   userAddress?: string
 ) => {
+  return async (
+    dispatch: Dispatch<UserActions> & Dispatch<DropActions>,
+    getState: () => RootState
+  ) => {
 
-  console.log({
-    connector,
-    userChainId,
-    userAddress
-  })
+    console.log({
+      connector,
+      userChainId,
+      userAddress
+    })
 
-  dispatch(actions.setInitialized(false))
+    dispatch(actions.setInitialized(false))
 
-  const {
-    drop: {
-      chainId: linkChainId,
-      campaignId,
-      linkdropMasterAddress,
-      linkId
+    const {
+      drop: {
+        chainId: linkChainId,
+        campaignId,
+        linkdropMasterAddress,
+        linkId
+      }
+    } = getState()
+
+    if (!campaignId) {
+      return onReload && onReload() 
     }
-  } = getState()
 
-  if (!campaignId) {
-    return onReload && onReload() 
+    if (!REACT_APP_INFURA_ID) {
+      return alert('REACT_APP_INFURA_ID is not provided in .env file')
+    }
+
+    if (!linkChainId) {
+      return alert('chainId of link is not provided in store')
+    }
+
+    if (!linkId) {
+      return alert('linkId of link is not provided in store')
+    }
+
+    if (!linkdropMasterAddress) {
+      return alert('linkdropMasterAddress of link is not provided in store')
+    }
+
+    if (!campaignId) {
+      return alert('campaignId of link is not provided in store')
+    }
+
+    const jsonRpcUrl = defineJSONRpcUrl({ chainId: Number(linkChainId), infuraPk: REACT_APP_INFURA_ID })
+    const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl)
+    dispatch(actions.setProvider(provider))
+
+    const claimed = await checkIfClaimed(
+      provider,
+      Number(linkChainId),
+      linkId,
+      linkdropMasterAddress,
+      campaignId
+    )
+
+    if (claimed) {
+      dispatch(actionsDrop.setIsClaimed(claimed))
+    }
+
+    if (!userChainId || !userAddress || !connector) {
+      dispatch(actions.setHasConnector(false))
+    } else {
+      dispatch(actions.setHasConnector(true))
+      dispatch(actions.setAddress(userAddress))
+      dispatch(actions.setChainId(userChainId))
+
+      const provider = await connector.getProvider()
+      const signer = await connector.getSigner()
+      
+      dispatch(actions.setUserProvider(provider))
+      dispatch(actions.setSigner(signer))
+    }
+
+    dispatch(actions.setInitialized(true))
   }
-
-  if (!REACT_APP_INFURA_ID) {
-    return alert('REACT_APP_INFURA_ID is not provided in .env file')
-  }
-
-  if (!linkChainId) {
-    return alert('chainId of link is not provided in store')
-  }
-
-  if (!linkId) {
-    return alert('linkId of link is not provided in store')
-  }
-
-  if (!linkdropMasterAddress) {
-    return alert('linkdropMasterAddress of link is not provided in store')
-  }
-
-  if (!campaignId) {
-    return alert('campaignId of link is not provided in store')
-  }
-
-  const jsonRpcUrl = defineJSONRpcUrl({ chainId: Number(linkChainId), infuraPk: REACT_APP_INFURA_ID })
-  const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl)
-  dispatch(actions.setProvider(provider))
-
-  const claimed = await checkIfClaimed(
-    provider,
-    Number(linkChainId),
-    linkId,
-    linkdropMasterAddress,
-    campaignId
-  )
-
-  alert(`claimed: ${claimed}`)
-
-  if (claimed) {
-    dispatch(actionsDrop.setIsClaimed(claimed))
-  }
-
-  if (!userChainId || !userAddress || !connector) {
-    dispatch(actions.setHasConnector(false))
-  } else {
-    dispatch(actions.setHasConnector(true))
-    dispatch(actions.setAddress(userAddress))
-    dispatch(actions.setChainId(userChainId))
-
-    const provider = await connector.getProvider()
-    const signer = await connector.getSigner()
-    
-    dispatch(actions.setUserProvider(provider))
-    dispatch(actions.setSigner(signer))
-  }
-
-  dispatch(actions.setInitialized(true))
 }
 
 export default initialize
