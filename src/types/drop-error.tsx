f308af8c47d410edbfb0716625be0faf571010ec
spacehaz <spacehaz@gmail.com>
2023-07-17 19:08:19 +0200

    added check for qr campaign is paused

--- src/types/drop-error.tsx
+++ src/types/drop-error.tsx
@@ -5,6 +5,7 @@ type TDropError = 'qr_error' |
                   'qr_incorrect_parameter' |
                   'qr_campaign_not_started' |
                   'qr_campaign_finished' |
-                  'qr_no_links_to_share'
+                  'qr_no_links_to_share' |
+                  'qr_campaign_not_active'
 
 export default TDropError
