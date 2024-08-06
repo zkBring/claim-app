a57a68137720fba6de5e1ca6bdf24a84ec38c504
spacehaz <spacehaz@gmail.com>
2024-08-06 13:40:26 +0300

    added updates for claim app bug

--- src/data/api/get-multiqr-data.tsx
+++ src/data/api/get-multiqr-data.tsx
@@ -5,16 +5,22 @@ const getMultiQRData = (
   multiscanQRId: string,
   scanId: string,
   scanIdSig: string,
+  msg?: string,
   chainId?: number,
   receiver?: string,
-  whitelistSig?: string
+  whitelistSig?: string,
+  nonce?: string,
+  timestamp?: number
 ) => {
   return axios.post(`${REACT_APP_DASHBOARD_SERVER_URL}/api/v2/dashboard/dispensers/pop/multiscan-qrs/${multiscanQRId}`, {
     scan_id: scanId,
     scan_id_sig: scanIdSig,
     receiver,
+    message: msg,
     chain_id: chainId ? String(chainId) : undefined,
-    whitelist_sig: whitelistSig
+    whitelist_sig: whitelistSig,
+    nonce,
+    timestamp
   })
 }
 
