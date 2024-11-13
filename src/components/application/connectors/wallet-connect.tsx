46314f51640e225faec56fc502d2158feedd74b4
spacehaz <spacehaz@gmail.com>
2024-11-13 12:37:52 +0300

    updates for appkit

--- src/components/application/connectors/wallet-connect.tsx
+++ src/components/application/connectors/wallet-connect.tsx
@@ -32,8 +32,8 @@ const metadata = {
 }
 
 const networks = [
-  mainnet,
   base,
+  mainnet,
   polygon,
   immutableZkEvm,
   xLayer,
@@ -58,7 +58,8 @@ createAppKit({
   featuredWalletIds: [
     'fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa',
     'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-    '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369'
+    '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369',
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
   ],
   // @ts-ignore
   networks,
